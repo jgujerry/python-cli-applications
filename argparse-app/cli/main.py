0264c8153543b4da9ee1ee211626bd84992d580f
jgujerry <jianligu3@gmail.com>
2024-04-15 20:16:34 -0600

    Update cli doc

--- argparse-app/cli/main.py
+++ argparse-app/cli/main.py
@@ -11,7 +11,7 @@ def main():
     subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
 
     # Add sub-command
-    add_parser = subparsers.add_parser('add', help='Add a new login')
+    add_parser = subparsers.add_parser('add', help='Add a new credential')
     add_parser.add_argument('appname', type=str, help='Appname')
     add_parser.add_argument('username', type=str, help='Username')
     add_parser.add_argument('password', type=str, help='Password')
@@ -19,18 +19,18 @@ def main():
 
     # Get sub-command
     get_parser = subparsers.add_parser('get', help='Retrieve a password')
-    get_parser.add_argument('appname_or_username', type=str, help='appname/App name or Username')
+    get_parser.add_argument('appname_or_username', type=str, help='App name or Username')
     get_parser.set_defaults(func=safekey.get_password)
 
     # Update sub-command
     update_parser = subparsers.add_parser('update', help='Update a password')
-    update_parser.add_argument('appname_or_username', type=str, help='appname/App name or Username')
+    update_parser.add_argument('appname_or_username', type=str, help='App name or Username')
     update_parser.add_argument('new_password', type=str, help='New Password')
     update_parser.set_defaults(func=safekey.update_password)
 
     # Remove sub-command
     remove_parser = subparsers.add_parser('remove', help='Remove a password')
-    remove_parser.add_argument('appname_or_username', type=str, help='appname/App name or Username')
+    remove_parser.add_argument('appname_or_username', type=str, help='App name or Username')
     remove_parser.set_defaults(func=safekey.remove_password)
 
     # List sub-command
