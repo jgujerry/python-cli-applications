b00f8f4dc4f48d77f65a781b65b1c107ffaa3a9a
jgujerry <jianligu3@gmail.com>
2024-04-27 21:54:49 -0600

    Update typer app

--- typer-app/cli/main.py
+++ typer-app/cli/main.py
@@ -8,8 +8,32 @@ safekey = SafeKey()
 
 @app.command()
 def add(appname: str, username: str, password: str):
+    """Add a new password entry."""
     safekey.add_password(appname, username, password)
     
+
+@app.command()
+def get(appname: str):
+    """Get a password for an app."""
+    safekey.get_password(appname)
+
+
+@app.command()
+def list():
+    """List all passwords."""
+    safekey.list_passwords()
+
+
+@app.command()
+def update(appname: str, new_password: str):
+    """Update a password for an app."""
+    safekey.update_password(appname, new_password)
+
+
+@app.command()
+def remove(appname: str):
+    """Remove a password for an app."""
+    safekey.remove_password(appname)
 
 
 
