2fcfe9bb9c582ae0c9bf429746bb602bb3a38063
jgujerry <jianligu3@gmail.com>
2024-04-28 19:59:13 -0600

    Add cli options

--- typer-app/cli/main.py
+++ typer-app/cli/main.py
@@ -7,13 +7,17 @@ safekey = SafeKey()
 
 
 @app.command()
-def add(appname: str, username: str, password: str):
+def add(
+    appname: str = typer.Option(None, "-a", "--appname", help="The name of the app."),
+    username: str = typer.Option(None, "-u", "--username", help="The username for the app."),
+    password: str = typer.Option(None, "-p", "--password", help="The password for the app."),
+):
     """Add a new password entry."""
     safekey.add_password(appname, username, password)
     
 
 @app.command()
-def get(appname: str):
+def get(appname: str = typer.Option(None, "-a", "--appname", help="The name of the app.")):
     """Get a password for an app."""
     safekey.get_password(appname)
 
@@ -25,13 +29,16 @@ def list():
 
 
 @app.command()
-def update(appname: str, new_password: str):
+def update(
+    appname: str = typer.Option(None, "-a", "--appname", help="The name of the app."),
+    new_password: str = typer.Option(None, "-p", "--password", help="The new password for the app."),
+):
     """Update a password for an app."""
     safekey.update_password(appname, new_password)
 
 
 @app.command()
-def remove(appname: str):
+def remove(appname: str = typer.Option(None, "-a", "--appname", help="The name of the app.")):
     """Remove a password for an app."""
     safekey.remove_password(appname)
 
