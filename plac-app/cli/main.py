e95ab6753e67ff7eeccee3cecc38a52080a7327e
jgujerry <jianligu3@gmail.com>
2024-05-18 22:06:30 -0600

    Update plac app

--- plac-app/cli/main.py
+++ plac-app/cli/main.py
@@ -1,98 +1,68 @@
-# cli.py
-import sys
+import os
+import shelve
 import plac
 from safekey.app import SafeKey
 
 
-# @plac.opt('--appname', "Application name")
-# @plac.opt('--username', "User name")
-# @plac.opt('--password', "Password")
-# def add(appname: str, username: str, password: str):
-#     sk = SafeKey()
-#     print(sk.add_password(appname, username, password))
+class ShelveInterface(object):
+    "A minimal interface over a shelve object."
+    commands = 'add', 'get', 'list', 'update', 'remove'
 
+    @plac.annotations(configfile=('path name of the shelve', 'option'))
+    def __init__(self, configfile):
+        self.configfile = configfile or 'conf.shelve'
+        self.fname = os.path.expanduser(self.configfile)
+        self.__doc__ += ('\nOperating on %s.\nUse help to see '
+                         'the available commands.\n' % self.fname)
 
-# @plac.opt('--appname', "Application name")
-# def get(appname: str):
-#     sk = SafeKey()
-#     print(sk.get_password(appname))
+    def __enter__(self):
+        self.sh = shelve.open(self.fname)
+        return self
 
+    def __exit__(self, etype, exc, tb):
+        self.sh.close()
+    
+    def add(self, appname, username, password):
+        """Add new password"""
+        sk = SafeKey()
+        print(sk.add_password(appname, username, password))
+    
+    def get(self, appname):
+        """Get the password"""
+        sk = SafeKey()
+        print(sk.get_password(appname))
+    
+    def list(self):
+        sk = SafeKey()
+        print(sk.list_passwords())
+    
+    def update(self, appname, new_password):
+        sk = SafeKey()
+        print(sk.update_password(appname, new_password))
+    
+    def remove(self, appname):
+        sk = SafeKey()
+        print(sk.remove_password(appname))
+    
+    def set(self, name, value):
+        "set name value"
+        yield 'setting %s=%s' % (name, value)
+        self.sh[name] = value
 
-# def list_():
-#     sk = SafeKey()
-#     print(sk.list_passwords())
+    def show(self, *names):
+        "show given parameters"
+        for name in names:
+            yield '%s = %s' % (name, self.sh[name])  # no error checking
 
-
-# @plac.opt('--appname', "Application name")
-# @plac.opt('--new-password', "Application name")
-# def update(appname: str, new_password: str):
-#     sk = SafeKey()
-#     print(sk.update_password(appname, new_password))
-
-
-# @plac.opt('--appname', "Application name")
-# def remove(appname: str):
-#     sk = SafeKey()
-#     print(sk.remove_password(appname))
-
-
-# def main():
-#     if len(sys.argv) > 1:
-#         command = sys.argv[1].strip()
-#     else:
-#         print("Usage: skcli [command]")
-#         print("Commands: add, get, list, update, remove")
-#         return
-
-#     commands = {
-#         'add': add,
-#         'get': get,
-#         'list': list_,
-#         'update': update,
-#         'remove': remove
-#     }
-
-#     if command in commands:
-#         plac.call(commands[command])
-#     else:
-#         print("Unknown command. Available commands: add, get, list, update, remove")
-
-
-# if __name__ == '__main__':
-#     main()
-
-# import plac
-
-# # Add decorators to the function
-# @plac.pos('model', help="model name", choices=['A', 'B', 'C'])
-# @plac.opt('iter', help="iterations", type=int)
-# @plac.flg('debug', help="debug mode")
-# def main(model, iter=100, debug=False):
-#     """
-#     A script for machine learning
-#     """
-#     print (model, iter, debug)
-
-# if __name__ == '__main__':
-#     # Execute function via plac.call().
-#     plac.call(main)
-
-
-import plac
-
-@plac.pos('model', help="model name", choices=['A', 'B', 'C'])
-@plac.opt('iter', help="iterations", type=int)
-@plac.flg('debug', help="debug mode")
-def demo(model, iter=100, debug=False):
-    """
-    A script for machine learning
-    """
-    print (model, iter, debug)
+    def showall(self):
+        "show all parameters"
+        for name in self.sh:
+            yield '%s = %s' % (name, self.sh[name])
 
 
 def main():
-    # Execute function via plac.call()
-    plac.call(demo)
+    plac.Interpreter(plac.call(ShelveInterface)).interact()
+
 
 
 if __name__ == '__main__':
