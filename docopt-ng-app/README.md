bc1df53bf9b2338702e6f4f1c9afe0efd1ce2590
jgujerry <jianligu3@gmail.com>
2024-06-11 22:27:32 -0600

    Update docopt-ng app

--- docopt-ng-app/README.md
+++ docopt-ng-app/README.md
@@ -36,5 +36,24 @@ Use the commands by following the `--help` instruction,
 
 ```bash
 $ skcli --help
+skcli - A simple CLI password manager.
 
+Usage:
+  skcli add <appname> <username> <password>
+  skcli get <appname>
+  skcli list
+  skcli update <appname> <new_password>
+  skcli remove <appname>
+  skcli (-h | --help)
+  skcli (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show the version.
+Commands:
+  add       Add a new password entry.
+  get       Retrieve a password entry.
+  list      List all password entries.
+  update    Update an existing password entry.
+  remove    Remove a password entry.
 ```
--- docopt-ng-app/cli/main.py
+++ docopt-ng-app/cli/main.py
@@ -1,23 +1,44 @@
+"""
+skcli - A simple CLI password manager.
+
+Usage:
+  skcli add <appname> <username> <password>
+  skcli get <appname>
+  skcli list
+  skcli update <appname> <new_password>
+  skcli remove <appname>
+  skcli (-h | --help)
+  skcli (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show the version.
+Commands:
+  add       Add a new password entry.
+  get       Retrieve a password entry.
+  list      List all password entries.
+  update    Update an existing password entry.
+  remove    Remove a password entry.
+"""
+
 import sys
 from docopt import docopt
 from safekey.app import SafeKey
 
-
 def main():
-    arguments = docopt(__doc__, version='SafeKey 1.0')
-
+    args = docopt(__doc__, version="v0.0.1")
     sk = SafeKey()
 
-    if arguments['add']:
-        sk.add_password(arguments['<appname>'], arguments['<username>'], arguments['<password>'])
-    elif arguments['get']:
-        sk.get_password(arguments['<appname>'])
-    elif arguments['list']:
+    if args['add']:
+        sk.add_password(args['<appname>'], args['<username>'], args['<password>'])
+    elif args['get']:
+        sk.get_password(args['<appname>'])
+    elif args['list']:
         sk.list_passwords()
-    elif arguments['update']:
-        sk.update_password(arguments['<appname>'], arguments['<new_password>'])
-    elif arguments['remove']:
-        sk.remove_password(arguments['<appname>'])
+    elif args['update']:
+        sk.update_password(args['<appname>'], args['<new_password>'])
+    elif args['remove']:
+        sk.remove_password(args['<appname>'])
 
 
 if __name__ == '__main__':
